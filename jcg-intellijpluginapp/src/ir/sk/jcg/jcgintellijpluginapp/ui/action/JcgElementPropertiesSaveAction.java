1b706377b5fa13ee89118996eaf47bc138120966
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2016-05-13 23:03:06 +0430

    Relationship complete.

--- jcg-intellijpluginapp/src/ir/sk/jcg/jcgintellijpluginapp/ui/action/JcgElementPropertiesSaveAction.java
+++ jcg-intellijpluginapp/src/ir/sk/jcg/jcgintellijpluginapp/ui/action/JcgElementPropertiesSaveAction.java
@@ -23,8 +23,24 @@ public class JcgElementPropertiesSaveAction extends NodeAction {
     public void actionPerformed(AnActionEvent e) {
         JcgProjectComponent jcgProjectComponent = JcgProjectComponent.getInstance(e.getProject());
 
+        jcgProjectComponent.setPropertiesModifiedElement();
+
+        doBeforeSave(jcgProjectComponent);
+
+        try {
+            jcgProjectComponent.getCodeGenerator().marshallingProject();
+        } catch (JAXBException e1) {
+            e1.printStackTrace();
+            logger.error("buildTemplate error in template : " + e);
+        }
+        jcgProjectComponent.reloadJcgTree(jcgProjectComponent.getSelectionPath());
+    }
+
+    /**
+     * Do stuffs need tob done before save like bidirectional relationships
+     * */
+    private void doBeforeSave(JcgProjectComponent jcgProjectComponent) {
         Element element = (Element) jcgProjectComponent.currentSelectedNodeUserObject();
-        CodeGenerator codeGenerator = jcgProjectComponent.getCodeGenerator();
 
         if (element instanceof Relationship) {
             TreePath treePath = jcgProjectComponent.getSelectionPath();
@@ -33,15 +49,20 @@ public class JcgElementPropertiesSaveAction extends NodeAction {
 
             Relationship relationship = (Relationship) element;
             RelationshipType relationshipType = relationship.getRelationshipType();
-            if (relationshipType == RelationshipType.ONE_TO_ONE_BIDIRECTIONAL
-                    || relationshipType == RelationshipType.MANY_TO_ONE_BIDIRECTIONAL
-                    || relationshipType == RelationshipType.MANY_TO_MANY_BIDIRECTIONAL) {
+            if (!relationship.getTargetEntity().equals(parentEntity) && (relationshipType.equals(RelationshipType.ONE_TO_ONE_BIDIRECTIONAL)
+                    || relationshipType.equals(RelationshipType.ONE_TO_MANY_BIDIRECTIONAL)
+                    || relationshipType.equals(RelationshipType.MANY_TO_ONE_BIDIRECTIONAL)
+                    || relationshipType.equals(RelationshipType.MANY_TO_MANY_BIDIRECTIONAL))) {
                 Entity targetEntity = relationship.getTargetEntity();
                 Relationship targetRelationship = new Relationship();
+                targetRelationship.setName(relationship.getName());
                 targetRelationship.setTargetEntity(parentEntity);
                 switch (relationshipType) {
                     case ONE_TO_ONE_BIDIRECTIONAL:
                         targetRelationship.setRelationshipType(RelationshipType.ONE_TO_ONE_BIDIRECTIONAL);
+                        break;
+                    case ONE_TO_MANY_BIDIRECTIONAL:
+                        targetRelationship.setRelationshipType(RelationshipType.MANY_TO_ONE_BIDIRECTIONAL);
                         break;
                     case MANY_TO_ONE_BIDIRECTIONAL:
                         targetRelationship.setRelationshipType(RelationshipType.ONE_TO_MANY_BIDIRECTIONAL);
@@ -53,15 +74,5 @@ public class JcgElementPropertiesSaveAction extends NodeAction {
                 targetEntity.addRelation(targetRelationship);
             }
         }
-
-        jcgProjectComponent.setPropertiesModifiedElement();
-
-        try {
-            jcgProjectComponent.getCodeGenerator().marshallingProject();
-        } catch (JAXBException e1) {
-            e1.printStackTrace();
-            logger.error("buildTemplate error in template : " + e);
-        }
-        jcgProjectComponent.reloadJcgTree(jcgProjectComponent.getSelectionPath());
     }
 }
